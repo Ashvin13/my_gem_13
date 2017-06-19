d68c76cb112474f4c32c8bd08d5b6f6f32cd7936
Patel Harshad <harshadce2008@gmail.com>
2017-06-19 18:52:01 +0530

    config file changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.asset_host = "https://powerful-depths-20638.herokuapp.com/"
+
   config.action_mailer.default_url_options = { host: 'https://powerful-depths-20638.herokuapp.com/', only_path: false }
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
