6081f40c278a2b75284faf44b2ee8b54874adc93
Patel Harshad <harshadce2008@gmail.com>
2017-06-19 17:08:49 +0530

    add rake file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616130739) do
+ActiveRecord::Schema.define(version: 20170619080115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170616130739) do
     t.boolean  "recipes_type"
     t.text     "description"
     t.string   "email"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170616130739) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
